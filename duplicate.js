028ec1ea7e4acb163f8aeecb6f48678d9dba8d76
Your Name <you@example.comcd>
2026-01-12 02:05:19 +0600

    removing duplicate item from an array

--- duplicate.js
+++ duplicate.js
@@ -1,2 +1,26 @@
-//if there is a duplicate array check it and remove it 
-const birayani khor
+//if there is a duplicate array check it and remove it
+const numbers = [12, 11, 12, 32, 32, 25, 78, 52, 11, 78];
+function noDuplicate(array) {
+  const unique = [];
+  for (const number of array) {
+    if (unique.includes(number) === false) {
+      unique.push(number);
+    }
+  }
+  return unique;
+}
+// const uniqueArray = noDuplicate(numbers);
+// console.log(uniqueArray);
+
+const birayaniKhor = ["abul", "sabul", "kabul", "abul", "mokbul", "kabul"];
+function noDuplicate(array) {
+  const unique = [];
+  for (const item of array) {
+    if (unique.includes(item) === false) {
+      unique.push(item);
+    }
+  }
+  return unique;
+}
+const uniqueArray = noDuplicate(birayaniKhor);
+console.log(uniqueArray);
