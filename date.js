c72233e768f5f5b64436c8f3aaec3e12c0831b15
Your Name <you@example.comcd>
2026-01-12 22:17:10 +0600

    learned swaping technique

--- date.js
+++ date.js
@@ -1,1 +1,11 @@
-// 
+const today = new Date();
+const date = new Date('2062-10-18')
+console.log(date.getMonth());
+console.log(date.getDay());
+const specificDate = new Date(2091, 0, 26);
+console.log(specificDate);
+specificDate.setMonth(10);
+console.log(specificDate.toLocaleString('en-GB'));
+
+//1970 saler ekhn porjonto ekhn porjonto koto second hoise ejonne eta kora 
+//etake bole -unix epocs
---
+++ swap.js
@@ -0,0 +1,18 @@
+let a = 7;
+let b = 5;
+
+console.log(a, b);
+const temp = a;
+a = b;
+b = temp;
+console.log(a, b);
+
+//-----------------------------
+let x = 5 ;
+let y = 7;
+console.log(x, y);
+[x, y] = [y, x];
+console.log(x,y);;
+
+//-----------------------------
+
