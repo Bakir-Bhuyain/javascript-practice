0197863c4717061a2f6dcb0031a1af33f9720a68
Your Name <you@example.comcd>
2026-01-12 21:16:54 +0600

    using Math function

--- duplicate.js
+++ duplicate.js
@@ -1,5 +1,5 @@
 //if there is a duplicate array check it and remove it
-const numbers = [12, 11, 12, 32, 32, 25, 78, 52, 11, 78];
+// const numbers = [12, 11, 12, 32, 32, 25, 78, 52, 11, 78];
 function noDuplicate(array) {
   const unique = [];
   for (const number of array) {
@@ -22,5 +22,34 @@ function noDuplicate(array) {
   }
   return unique;
 }
-const uniqueArray = noDuplicate(birayaniKhor);
-console.log(uniqueArray);
+// const uniqueArray = noDuplicate(birayaniKhor);
+// console.log(uniqueArray);
+
+/**Scenario: একটা কমেন্টের লিস্ট আছে। সেখানে কিছু খারাপ কথা ("bad") আছে। তোমাকে সেগুলো বাদ দিয়ে একটা Clean List বানাতে হবে।*/
+const comments = ["good", "bad", "nice", "bad", "excellent"];
+function filtering(bad) {
+  const newWords = [];
+  for (const item of bad) {
+    if (newWords.includes(item) === false) {
+      newWords.push(item);
+    }
+  }
+  return newWords;
+}
+// const newArray = filtering(comments);
+// console.log(newArray)
+
+const numbers = [10, 55, 2, 90, 40];
+let biggest = numbers[0];
+
+function findBigNumber(numberList) {
+  for (const num of numberList) {
+    if (num > biggest) {
+      biggest = num;
+    }
+  }
+  return biggest;
+}
+
+const result = findBigNumber(numbers);
+console.log(result);
---
+++ math.js
@@ -0,0 +1,14 @@
+ const min = Math.min(45, 2 ,4, 54, 1, 7, 6, 7);
+ console.log(min)
+
+ const max = Math.max(45, 2, 4, 54, 1, 7, 6, 7);
+ console.log(max);
+
+ console.log(Math.PI);
+ console.log(Math.abs(-88));
+ console.log(Math.round(2.25));
+ console.log(Math.ceil(2.999));
+ console.log(Math.random());
+ console.log(Math.random()*10);
+ const rand = Math.round(Math.random() * 10);
+ console.log(rand);
